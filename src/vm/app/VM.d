a6adff7a8b17b747c46e618f14d2f91c8ffd1bac
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-22 21:54:07 +0000

    .

--- src/vm/app/VM.d
+++ src/vm/app/VM.d
@@ -1,36 +1,32 @@
 module app.VM;
 
 public struct Proc {
-  ulong r0,r1,r2,r3;
+    ulong r0,r1,r2,r3;
     ulong cp;   //   Call pointer.  
     ulong dp;   //   Data pointer.  
     ulong fr;   //   Flag register.  
     ulong kr;   //   Kern-register.  
     ulong ip;   //   Instruction pointer.  
     ulong rp;   //   Result pointer.  
-  public ubyte[] mem;
+    void[] mem;
 };
 
 public class VM {
 	public static bool run= true;
 	public static bool allowFullIO= false;
 	public static bool allowNativeExt= true;
-	public static Proc proc= Proc();
+	public Proc proc= Proc();
 	public static ulong limit= 400;
 	public static void function()[256] vm,vm_ff;
 	private static void f_00() {};
-    private static void f_ff() {
-        
-    };
-	private void f_ff_00() {
-    // this.proc.mem += Proc();
-  };
+    private static void f_ff() {};
+	private void f_ff_00() {};
 	static this() {
 		this.vm[0x00]= &f_00;
 		this.vm[0xff]= &f_ff;
 		this.vm_ff[0x00]= &f_ff_00;
 	};
 	public this(ubyte[] mem) {
-    this.proc.mem= mem;
-  };
+        this.proc= Proc(0,0,0,0, 0,0, 0, 0, 0, 0, cast(void[]) men);
+    };
 };
