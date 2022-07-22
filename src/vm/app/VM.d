0c506c6f9e4704e9ca5139a742881d7a50ed8520
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-22 21:59:32 +0000

    .

--- src/vm/app/VM.d
+++ src/vm/app/VM.d
@@ -17,10 +17,10 @@ public class VM {
 	public static bool allowNativeExt= true;
 	public Proc proc= Proc();
 	public static ulong limit= 400;
-	public static void function()[256] vm,vm_ff;
-	private static void f_00() {};
-    private static void f_ff() {};
-	private void f_ff_00() {};
+	public static void function(Proc)[256] vm,vm_ff;
+	private static void f_00(Proc) {};
+    private static void f_ff(Proc) {};
+	private void f_ff_00(Proc) {};
 	static this() {
 		this.vm[0x00]= &f_00;
 		this.vm[0xff]= &f_ff;
@@ -28,5 +28,10 @@ public class VM {
 	};
 	public this(ubyte[] mem) {
         this.proc= Proc(0,0,0,0, 0,0, 0, 0, 0, 0, cast(void[]) men);
+    };
+    public void initThread(string objPath) {
+        while(this.running) {
+            this.vm[this.proc.ip++](this.proc);
+        };
     };
 };
