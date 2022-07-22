1f1c6737713f760fc5f893b78de74ac84157e562
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-22 14:51:42 +0000

    Setup project.

---
+++ replit.nix
@@ -0,0 +1,6 @@
+{ pkgs }: {
+    deps = with pkgs;
+    [
+        ldc
+    ];
+}
---
+++ vm.d
@@ -0,0 +1,8 @@
+module vm;
+
+public class vm {};
+
+void main(string[] ArgV) {
+    import std.stdio;
+    writeln("Hello world.  ");
+};
