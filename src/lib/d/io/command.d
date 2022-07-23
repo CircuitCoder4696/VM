dacb68cffe31d19bb67ccf7bbec2bffb7b4d4e3f
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-23 00:38:44 +0000

    Error accorded in replit.  :/  It froze.

--- src/lib/d/io/command.d
+++ src/lib/d/io/command.d
@@ -2,8 +2,16 @@ module d.io.command;
 
 public class ArgHandler {
     private string[] ArgV;
+    public this()
     public static string[] opDispatch(string param)() {
+        bool appendArguments= false;
         string[] result;
-        foreach(a; this.ArgV)if(a[0]=='-') {} else {};
+        foreach(a; this.ArgV)if(a[0]=='-') {
+            appendArguments= false;
+            if(a[1..$]==param)appendArguments= true;
+        } else {
+            if(appendArguments)result ~= [a];
+        };
+        return result;
     };
 };
