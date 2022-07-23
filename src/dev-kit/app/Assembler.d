df0311ad8d9dd194dd1e25e0e5857b5875f20698
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-23 04:58:02 +0000

    .

--- src/dev-kit/app/Assembler.d
+++ src/dev-kit/app/Assembler.d
@@ -15,11 +15,23 @@ public class Assembler {
         };
         this.srcCode= (cast(string) read(filePath)).split("\n");
     };
+    public bool assembleInst(string inst) {
+        import std.array;
+        import std.stdio:writeln;
+        int instI= 0;
+        while(inst[instI]==0x20&&instI<inst.length)instI++;
+        string[] instSegs= inst[instI..$].split(" ");
+        switch(instSegs[0]) {
+            case "obj":
+                writeln("object path: ",(inst[(instI +4)..$]),";");
+        };
+    };
     public typeof(this) assemble() {
         import std;
         ubyte[] result= [];
-        foreach(inst; this.srcCode)switch(inst) {
-            default: writeln("Unknown instruction:   ",inst);
+        foreach(inst; this.srcCode) {
+            if(assembleInst(inst))continue;
+            writeln("Unknown instruction:   ",inst);
         };
         this.data= cast(void[]) result;
         return this;
