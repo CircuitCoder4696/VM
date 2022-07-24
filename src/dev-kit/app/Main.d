3474389c2973482d48142d6bb946281039c92c3d
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-24 19:44:00 +0000

    :/

--- src/dev-kit/app/Main.d
+++ src/dev-kit/app/Main.d
@@ -1,19 +1,22 @@
 module app.Main;
 import d.proc.command:ArgHandler;
+import templates.DevToolChain;
 
-public void main(string[] ArgV) {
-    ArgHandler ah= new ArgHandler(ArgV, "c:compile", "o:output", "args:args");
-    import app.Assembler;
-    import std.stdio:writeln;
-    Assembler assembly;
-    foreach(sourceFile; ah.getParam("compile")) {
-        assembly= new Assembler(sourceFile);
-        log3.info(assembly.assemble().data);
-        version(dbg_00)writeln("defined symbols= ",assembly.symbols,";");
-    };
-    writeln("/----- (0)");
+public class Main:DevToolChain {
+    public static void main(string[] ArgV) {
+        ArgHandler ah= new ArgHandler(ArgV, "c:compile", "o:output", "args:args");
+        import app.Assembler;
+        Assembler assembly;
+        foreach(sourceFile; ah.getParam("compile")) {
+            assembly= new Assembler(sourceFile);
+            log3.info(assembly.assemble().data);
+            version(dbg_00)writeln("defined symbols= ",assembly.symbols,";");
+        };
+    qStart0();
     log.info(ah.getParam("compile"));
-    writeln("\\----- (0)");
+    qStop0();
     log.info(ah.getParam("args"));
     log.info("Hello vmdk.  ");
+};
+
 };
