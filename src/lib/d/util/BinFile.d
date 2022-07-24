e1b9f0467f6945586d488e40ecf486dcd61deb85
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-24 01:54:56 +0000

    .

--- src/lib/d/util/BinFile.d
+++ src/lib/d/util/BinFile.d
@@ -6,6 +6,7 @@ public class FileFormat {
     import std.stdio:writeln;
     private BinFile parent;
     public string[] symbols= [];
+    public string[string] aliases;
     public string[][string] params;
     private enum:uint {
         Executable,
