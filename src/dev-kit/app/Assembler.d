750888408165188d80280906dfd3725744be89e4
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-24 17:02:12 +0000

    :/

--- src/dev-kit/app/Assembler.d
+++ src/dev-kit/app/Assembler.d
@@ -1,10 +1,11 @@
 module app.Assembler;
 
-version(dbg_01)private enum dbg= 1;
-version(dbg_02)private enum dbg= 2;
-version(dbg_03)private enum dbg= 3;
-version(dbg_04)private enum dbg= 4;
-version(dbg_05)private enum dbg= 5;
+version(dbg_01){private enum dbg= 1;}else
+version(dbg_02){private enum dbg= 2;}else
+version(dbg_03){private enum dbg= 3;}else
+version(dbg_04){private enum dbg= 4;}else
+version(dbg_05){private enum dbg= 5;}else
+{ private enum dbg= 0; }
 
 public abstract class a0Assembler {
     import d.util.BinFile;
