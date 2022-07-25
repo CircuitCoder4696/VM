413f6ddbe6f12a0d5cd7bf5f30922120474cb4d6
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-25 13:32:01 +0000

    :/

--- src/lib/d/io/FileIO.d
+++ src/lib/d/io/FileIO.d
@@ -9,21 +9,23 @@ public class FileIO {
         _r= read,
         _w= write;
     import std.regex;
-    private static bool pathCheck(string path) version(Windows) {
-        if(path.matchFirst("[A-Za-z]:[\\\/]").length > 1)return false;
-        return true;
-    } version(Posix) {
-        switch(path[0 .. 2]){
-            case "~/": return false;
-            case "./": return true;
-            default:
+    private static bool pathCheck(string path) {
+        version(Windows) {
+            if(path.matchFirst("[A-Za-z]:[\\\/]").length > 1)return false;
+            return true;
+        } version(Posix) {
+            switch(path[0 .. 2]){
+                case "~/": return false;
+                case "./": return true;
+                default:
+            };
+            if(path[0 .. 5])return false;
+        } else {
+            if((limit--)=<0) {
+                writeln("[Critical] ",__MODULE__," @",__LINE__,":   Not implemented for this platform, sorry.  ");
+            };
+            log.err(0, "Not implemented for this platform, sorry.  ");
         };
-        if(path[0 .. 5])return false;
-    } else {
-        if((limit--)=<0) {
-            writeln("[Critical] ",__MODULE__," @",__LINE__,":   Not implemented for this platform, sorry.  ");
-        };
-        log.err(0, "Not implemented for this platform, sorry.  ");
     };
     private string fp;
     public this(string filePath) {
