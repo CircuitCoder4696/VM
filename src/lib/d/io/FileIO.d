34de0f8c3d1ba24158f599cff5f0e6e572468079
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-25 13:33:38 +0000

    :/

--- src/lib/d/io/FileIO.d
+++ src/lib/d/io/FileIO.d
@@ -11,7 +11,7 @@ public class FileIO {
     import std.regex;
     private static bool pathCheck(string path) {
         version(Windows) {
-            if(path.matchFirst("[A-Za-z]:[\\\/]").length > 1)return false;
+            if(path.matchFirst("[A-Za-z]:[\\\\/]").length > 1)return false;
             return true;
         } version(Posix) {
             switch(path[0 .. 2]){
@@ -21,7 +21,7 @@ public class FileIO {
             };
             if(path[0 .. 5])return false;
         } else {
-            if((limit--)=<0) {
+            if((limit--) <= 0) {
                 writeln("[Critical] ",__MODULE__," @",__LINE__,":   Not implemented for this platform, sorry.  ");
             };
             log.err(0, "Not implemented for this platform, sorry.  ");
