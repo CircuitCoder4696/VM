23fcad561d6bcd747821fd99f0b2ce045da1c540
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-25 05:14:57 +0000

    .

--- src/dev-kit/app/Main.d
+++ src/dev-kit/app/Main.d
@@ -8,9 +8,11 @@ public class Main:DevToolChain {
         import app.Assembler;
         Assembler assembly;
         foreach(sourceFile; ah.getParam("compile")) {
+            start0();
             assembly= new Assembler(sourceFile);
             log.info(5, assembly.assemble().data);
             log.info(4, "defined symbols= ",assembly.symbols,";");
+            stop0();
         };
         log.info(0, ah.getParam("compile"));
         log.info(0, ah.getParam("args"));
