f00f92f05eb445fde60146e7d69722d1852d2a6d
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-07-30 05:02:40 +0000

    Oops.  :/

--- src/expiramental/Transclusion.d
+++ src/expiramental/Transclusion.d
@@ -5,7 +5,17 @@ public class Transclusion {
         public string remainder;
         public string[string] transcudedVals;
         public this(string main, string transcludeString) {
-            
+            string[string] transcusionVars;
+            string[][2] transcusionStructure= genTranscusionStructure(transcludeString);
+            string[] transcusionData= getTranscludedData(main, transcusionStructure);
+            transclusionVars= combineTransclusionParts(transcusionVars, transcusionData);
+            return transclusionVars;
         };
+    };
+};
+
+public class t_Transcusion {
+    public void test(string[] ArgV) {
+        auto v_00= Transcusion.transclude("print 2 + 8;", "print $(X) + $(Y);");
     };
 };
